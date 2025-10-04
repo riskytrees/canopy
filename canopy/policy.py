d1f6d8fd757cb4fb4438bd073a63076f167b87ae
Josiah Bruner <josiah@josiahbruner.com>
2025-10-04 16:34:39 -0400

    It is alive

--- canopy/policy.py
+++ canopy/policy.py
@@ -4,4 +4,10 @@ class CanopyPolicy():
         self.policy = policy
 
     def is_allowed(self, tool_call: str) -> bool:
-        return True
+        flows = self.policy.get("flows", {})
+        for flow_name, flow in flows.items():
+            allowed_calls = flow.get("allowed_calls", [])
+            if tool_call in allowed_calls:
+                return True
+
+        return False
